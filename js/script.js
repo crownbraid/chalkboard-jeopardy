f017521e8d0a3e5b11c44f834170f32228097667
oilylyme <elynorfreyss@gmail.com>
2017-02-02 14:27:25 -0500

    reduced to clientside app for gh-pages demo

--- js/script.js
+++ js/script.js
@@ -50,6 +50,7 @@ function newGame() {
 	haltAni = 'on';
 	resetGame();
 	questsGet();
+	newQuestion();
 }
 function resetGame() {
 	$('.erase').finish().css('padding-top', '35em').show();
@@ -61,9 +62,14 @@ function resetGame() {
 	scoreAni('.turn-count', scInc);
 }
 function questsGet() {
-	$.get( "/questions/" + numQuests, function(data) {
-		gameQuests = data;
-		newQuestion();
+	// SELECT RANDOM QUESTION SET, LENGTH SPECIFIED BY "numQuests"
+	gameQuests = [];
+	while (gameQuests.length < numQuests) {
+		var numAdd = Math.floor(Math.random()*questions.length);
+		if (!gameQuests.includes(numAdd)) {gameQuests.push(numAdd);}
+	}
+	gameQuests = gameQuests.map(function(i) {
+		return questions[i];
 	});
 }
 
@@ -119,3 +125,23 @@ function endGame() {
 	$('main .answers').html($('.templates .answers').clone());
 	$('#question').text("You scored " + numCorr + " out of " + numQuests + "!");
 }
+
+// QUESTION OBJECT
+function question(question, answers, answer) {
+	this.question = question;
+	this.answers = answers;
+	this.answer = answer;
+}
+var questions = [
+	new question("How large was the first VCR, made in 1956? The size of a", ["piano", "sailboat", "oven", "yacht"], "piano"),
+	new question("Who invented roulette?", ["French mathematician Blaise Pascal", "Professional Gambler Jimmy Fitzgerald", "Aesthetician Christopher Dresser", "King Louis XIV"], "French mathematician Blaise Pascal"),
+	new question("Which publisher printed the first paperback book in 1935?", ["Penguin", "Dell", "Potter Press", "Pan Books"], "Penguin"),
+	new question("Which film director invented false eyelashes in 1916?", ["D.W. Griffith", "Ernst Lubitsch", "Victor Sjöström", "Leni Riefenstahl"], "D.W. Griffith"),
+	new question("Which root produces more distilled spirits than potatoes?", ["carrots", "ginger root", "beets", "turmeric"], "carrots"),
+	new question("Which US state produces two-thirds of the worlds egg plant crop?", ["New Jersey", "California", "New York", "Missouri"], "New Jersey"),
+	new question("Which country has the highest minimum drinking age in the world?", ["The United States", "Djibouti", "Singapore", "Qatar"], "The United States"),
+	new question("When a drunk driver is jailed in Malaysia, who is jailed along with them?", ["their spouse", "their father", "their lawyer", "any children of-age"], "their spouse"),
+	new question("Three live dogs have a star in the Hollywood walk of fame: Rin Tin Tin, Lassie, and who else?", ["Strongheart", "Toto", "Hooch", "Petey"], "Strongheart"),
+	new question("On average, how many minor earthquakes occur per minute?", [2, 0, 4, 6], 2),
+	new question("Which animal actors get paid the most, as much as 20,000 a day?", ["bears", "tigers", "elephants", "chimpanzees"], "bears")
+];
