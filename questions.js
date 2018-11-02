8be0f88aa22553a2dec00e124cb0d122ef088702
Elynor <elynorfreyss@gmail.com>
2018-11-01 23:40:15 -0400

    Modernize syntax

--- questions.js
+++ questions.js
@@ -1,47 +1,38 @@
-var unirest = require('unirest');
+const unirest = require('unirest');
 
-var NewQuestion = function() {
-	this.question = null;
-	this.answer = null;
-	this.fakeAnswers = [];
-}
+class Question {
+	constructor() {
+		unirest.get('http://jservice.io/api/random', function(req, res) {
+			this.question = req.body[0].question;
+			this.answer = req.body[0].answer;
+			this.getSimilarAnswers();
+		});
+	}
 
-NewQuestion.prototype.getSimilarAnswers = function() {
-	var url = 'https://wordsapiv1.p.mashape.com/words/'
-	var options = {'X-Mashape-Key': 'Q3mEcts35FmshxFGDp94g0ach9F0p1IfiAWjsnD2iNV7FBSpcG'};
+	getSimilarAnswers() {
+		const url = 'https://wordsapiv1.p.mashape.com/words/'
+		const options = {'X-Mashape-Key': 'Q3mEcts35FmshxFGDp94g0ach9F0p1IfiAWjsnD2iNV7FBSpcG'};
 
-	unirest.get(url + this.answer, options, function(req, res) {
-		req.results.forEach(function(result) {
-			unirest.get(url + result.typeOf, options, function(req, res) {
-				req.results.forEach(function(result) {
-					result.hasInstances.forEach(function(instance) {
-						this.fakeAnswers.push(instance);
+		unirest.get(url + this.answer, options, (req, res) => {
+			req.results.forEach( result => {
+				unirest.get(url + result.typeOf, options, (req, res) => {
+					req.results.forEach( result => {
+						result.hasInstances.forEach( instance => {
+							this.fakeAnswers.push(instance);
+						});
 					});
 				});
 			});
 		});
-	});
-}
-
-
-function createNewQuestionObject() {
-	var questionObject = new NewQuestion();
-
-	unirest.get('http://jservice.io/api/random', function(req, res) {
-		questionObject.question = req.body[0].question;
-		questionObject.answer = req.body[0].answer;
-		questionObject.getSimilarAnswers();
-	});
-
-	return questionObject;
-}
+	}
+};
 
 module.exports = {
 	store: [],
 	storeSize: 0,
-	reloadStore: function () {
+	reloadStore() {
 		while (this.store.length < this.storeSize) {
-			this.store.push(createNewQuestionObject());
+			this.store.push(new Question);
 		}
 	}
-}	
+}
